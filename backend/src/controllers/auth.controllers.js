47674a535a69d34233eb11ae3a2d3512ce5ac598
ShubhamOS-cmd <shubham19gupta9876@gmail.com>
2026-07-31 18:37:32 +0530

    remove mails

--- backend/src/controllers/auth.controllers.js
+++ backend/src/controllers/auth.controllers.js
@@ -118,7 +118,7 @@ const otpRequest = asyncHandler(async(req , res) => {
         `otp:${email}:${type}`,
         hashedOtp,
         'EX',
-        300
+        600
     )
 
     await emailQueue.add( // adding an email job to the Queue // internally does LPUSH to redis list 
@@ -239,29 +239,29 @@ const register = asyncHandler(async(req , res) => {
             7 * 24 * 60 * 60 
         ),
     ]);
-    await emailQueue.add( // adding an email job to the Queue
-      'Welcome-Email',
-      {
-        to: email,
-        subject: `Welcome ${userName}`,
-        body: `
-          <div>
-            <h2>Thanks ${userName} to choosing us</h2>
-            <h1>let's chat with your friend smoothly Feel free to contact us!</h1>
-          </div>
-        `,
-      },
-      {
-        attempts: 3, // if the job fails , BullMQ will retry it up to 3 times before marking it as failed 
-        backoff: { // control the delays b/w retries 
-          type: 'exponential',
-          delay: 2000,
-        },
-        // 1st fail -> wait 2000 retry 
-        // 2nd fail -> wait 4000 retry 
-        // 3rd fail -> wait 8000 retry 
-      }
-    );
+    // await emailQueue.add( // adding an email job to the Queue
+    //   'Welcome-Email',
+    //   {
+    //     to: email,
+    //     subject: `Welcome ${userName}`,
+    //     body: `
+    //       <div>
+    //         <h2>Thanks ${userName} to choosing us</h2>
+    //         <h1>let's chat with your friend smoothly Feel free to contact us!</h1>
+    //       </div>
+    //     `,
+    //   },
+    //   {
+    //     attempts: 3, // if the job fails , BullMQ will retry it up to 3 times before marking it as failed 
+    //     backoff: { // control the delays b/w retries 
+    //       type: 'exponential',
+    //       delay: 2000,
+    //     },
+    //     // 1st fail -> wait 2000 retry 
+    //     // 2nd fail -> wait 4000 retry 
+    //     // 3rd fail -> wait 8000 retry 
+    //   }
+    // );
     return res.status(201).json(new ApiResponse(201 , 
         createdUser , 
         "User created Successfully"
@@ -307,27 +307,27 @@ const login = asyncHandler(async(req , res) => {
             'EX',
             7 * 24 * 60 * 60 
     )
-    await emailQueue.add( // adding an email job to the Queue
-      'Recent-Login',
-      {
-        to: email,
-        subject: 'Recent Login ',
-        body: `
-          <div>
-            <h2>Hey ${user.userName} we see a new recent login</h2>
-            <h1>if this was you ignore this email</h1>
-            <p>otherwise go and check</p>
-          </div>
-        `,
-      },
-      {
-        attempts: 3, 
-        backoff: { // control the delays b/w retries 
-          type: 'exponential',
-          delay: 2000,
-        },
-      }
-    );
+    // await emailQueue.add( // adding an email job to the Queue
+    //   'Recent-Login',
+    //   {
+    //     to: email,
+    //     subject: 'Recent Login ',
+    //     body: `
+    //       <div>
+    //         <h2>Hey ${user.userName} we see a new recent login</h2>
+    //         <h1>if this was you ignore this email</h1>
+    //         <p>otherwise go and check</p>
+    //       </div>
+    //     `,
+    //   },
+    //   {
+    //     attempts: 3, 
+    //     backoff: { // control the delays b/w retries 
+    //       type: 'exponential',
+    //       delay: 2000,
+    //     },
+    //   }
+    // );
 
     return res.status(200).json(new ApiResponse(200 , 
         {
@@ -395,26 +395,26 @@ const changePassword = asyncHandler(async(req,res)=>{
       redis.del(`otp:${email}:password-reset:verified`),
       redis.del(`user:${user._id}:refresh-token`),
     ]);
-    await emailQueue.add( // adding an email job to the Queue
-      'password-change',
-      {
-        to: email,
-        subject: 'password-change',
-        body: `
-          <div>
-            <h2>Your Password is Changed</h2>
-            <p>Login Again </p>
-          </div>
-        `,
-      },
-      {
-        attempts: 3, // if the job fails , BullMQ will retry it up to 3 times before marking it as failed 
-        backoff: { // control the delays b/w retries 
-          type: 'exponential',
-          delay: 2000,
-        },
-      }
-    );
+    // await emailQueue.add( // adding an email job to the Queue
+    //   'password-change',
+    //   {
+    //     to: email,
+    //     subject: 'password-change',
+    //     body: `
+    //       <div>
+    //         <h2>Your Password is Changed</h2>
+    //         <p>Login Again </p>
+    //       </div>
+    //     `,
+    //   },
+    //   {
+    //     attempts: 3, // if the job fails , BullMQ will retry it up to 3 times before marking it as failed 
+    //     backoff: { // control the delays b/w retries 
+    //       type: 'exponential',
+    //       delay: 2000,
+    //     },
+    //   }
+    // );
     return res.status(200).json(new ApiResponse(200 , "Password change Successfully relogin now"));
 })
 const logout = asyncHandler(async(req , res)=>{
